2841607b9da76614af18efd7ac6402013baf82f1
richknicks <rlynch@gmail.com>
2020-12-23 02:42:01 -0500

    adding PORT

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(PORT, () => {
-  console.log(`**Server is running on http://localhost:${PORT}**`);
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`**Server is running on port ${process.env.PORT}`);
 });
