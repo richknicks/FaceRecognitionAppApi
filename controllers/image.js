89e26e4e07c84e8e272176c858dd7a5f31fc762e
richknicks <rlynch@gmail.com>
2020-12-23 00:48:00 -0500

    initial commit

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "80f4141be0554cd19497116e8d784e83",
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("Unable to work with API"));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
+};
+
+module.exports = {
+  handleImage,
+  handleApiCall,
+};
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+const knex = require("knex");
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "Ral1977$",
+    database: "smartbrain",
+  },
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.status(200).json("Success");
+});
+
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  profile.hanldleProfileGet(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.listen(PORT, () => {
+  console.log(`**Server is running on http://localhost:${PORT}**`);
+});
