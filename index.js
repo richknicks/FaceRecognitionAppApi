dad8851f2606407b18c02f57c9db50cfee0504f6
richknicks <rlynch@gmail.com>
2020-12-29 16:57:37 -0500

    added server script

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const db = knex({
 const app = express();
 app.use(express.json());
 app.use(cors());
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.status(200).json("Success");
