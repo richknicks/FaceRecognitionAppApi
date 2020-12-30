330faa3033471934a3ac1e674b9446e4c284cbb7
richknicks <rlynch@gmail.com>
2020-12-30 11:47:11 -0500

    fixed route .get

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const image = require("./controllers/image");
 //   },
 // });
 const { Pool } = require("pg");
-const db = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
@@ -28,20 +28,21 @@ server.use(cors());
 const PORT = process.env.PORT || 5000;
 
 server.get("/", (req, res) => {
-  res.status(200).json("Success");
-});
-
-server.get("/db", async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query("SELECT * FROM users");
-    const results = { results: result ? result.rows : null };
-    res.render("pages/db", results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
+  res
+    .status(200)
+    .json("Success")
+    .get("/db", async (req, res) => {
+      try {
+        const client = await pool.connect();
+        const result = await client.query("SELECT * FROM users");
+        const results = { results: result ? result.rows : null };
+        res.render("pages/db", results);
+        client.release();
+      } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+      }
+    });
 });
 
 server.post("/signin", (req, res) => {
