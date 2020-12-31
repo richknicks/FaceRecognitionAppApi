e4cedc6abbf7c3b36fda1d3434fd92516272ebac
richknicks <rlynch@gmail.com>
2020-12-31 01:23:13 -0500

    connected db

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ server.get("/", (req, res) => {
     .json("Success")
     .get("/db", async (req, res) => {
       try {
-        const client = await pool.connect();
+        const client = await db.connect();
         const result = await client.query("SELECT * FROM users");
         const results = { results: result ? result.rows : null };
         res.render("pages/db", results);
